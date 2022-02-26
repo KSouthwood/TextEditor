e757f22a4c2c7bb7f1412fa2a42c29f00382c0ca
Keri Southwood-Smith <keri.southwodo@gmail.com>
2022-02-26 12:37:36 -0500

    Complete Stage 1
    Add a JTextArea to the JFrame.

--- editor/TextEditor.java
+++ editor/TextEditor.java
@@ -1,12 +1,32 @@
 package editor;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TextEditor extends JFrame {
     public TextEditor() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 300);
+        setWindowProperties();
+        addComponents();
         setVisible(true);
-        setLayout(null);
+    }
+
+    private void setWindowProperties() {
+         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         setSize(300, 300);
+         setTitle("Text Editor");
+         setLocationRelativeTo(null);
+         setLayout(new BorderLayout());
+     }
+
+    private void addComponents() {
+        add(createTextArea(), BorderLayout.CENTER);
+    }
+
+    private JTextArea createTextArea() {
+        JTextArea jTextArea = new JTextArea();
+        jTextArea.setName("TextArea");
+        jTextArea.setLineWrap(true);
+        jTextArea.setWrapStyleWord(true);
+        return jTextArea;
     }
 }
