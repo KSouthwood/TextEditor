5e9c035aecd35da7c0661455a61df07352f70d7e
Keri Southwood-Smith <keri.southwodo@gmail.com>
2022-02-26 14:05:18 -0500

    Implement Stage 2
    Addition of the filename text field as well as load and save buttons.
    Just placement and size and making look pretty - adding the
    functionality will come next.

--- editor/TextEditor.java
+++ editor/TextEditor.java
@@ -1,9 +1,12 @@
 package editor;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.plaf.basic.BasicBorders;
 import java.awt.*;
 
 public class TextEditor extends JFrame {
+
     public TextEditor() {
         setWindowProperties();
         addComponents();
@@ -11,15 +14,23 @@ public class TextEditor extends JFrame {
     }
 
     private void setWindowProperties() {
-         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         setSize(300, 300);
-         setTitle("Text Editor");
-         setLocationRelativeTo(null);
-         setLayout(new BorderLayout());
-     }
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(450, 450);
+        setTitle("Text Editor");
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+    }
 
     private void addComponents() {
-        add(createTextArea(), BorderLayout.CENTER);
+        add(createPanel(), BorderLayout.NORTH);
+        add(createScrollPane(), BorderLayout.CENTER);
+    }
+
+    private JScrollPane createScrollPane() {
+        JScrollPane jScrollPane = new JScrollPane(createTextArea());
+        jScrollPane.setName("ScrollPane");
+        jScrollPane.setBorder(new EmptyBorder(10, 10, 10, 10));
+        return jScrollPane;
     }
 
     private JTextArea createTextArea() {
@@ -27,6 +38,41 @@ public class TextEditor extends JFrame {
         jTextArea.setName("TextArea");
         jTextArea.setLineWrap(true);
         jTextArea.setWrapStyleWord(true);
+        jTextArea.setBorder(BasicBorders.getTextFieldBorder());
         return jTextArea;
     }
+
+    private JPanel createPanel() {
+        JPanel jPanel = new JPanel();
+        jPanel.setLayout(new FlowLayout());
+        jPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        jPanel.add(createJTextField());
+        jPanel.add(createLoadButton());
+        jPanel.add(createSaveButton());
+        return jPanel;
+    }
+
+    private JTextField createJTextField() {
+        JTextField jTextField = new JTextField();
+        jTextField.setName("FilenameField");
+        jTextField.setPreferredSize(new Dimension(250, 30));
+        jTextField.setBorder(BasicBorders.getTextFieldBorder());
+        return jTextField;
+    }
+
+    private JButton createLoadButton() {
+        JButton loadButton = new JButton("Load");
+        loadButton.setName("LoadButton");
+        loadButton.setPreferredSize(new Dimension(75, 30));
+        return loadButton;
+    }
+
+    private JButton createSaveButton() {
+        JButton saveButton = new JButton("Save");
+        saveButton.setName("SaveButton");
+        saveButton.setPreferredSize(new Dimension(75, 30));
+        return saveButton;
+    }
+
+
 }
